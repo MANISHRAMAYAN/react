887977184babcd859e0b8a58d3496eb1262e4659
manish <manish@gmail.com>
2023-08-09 22:00:50 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, BrowserRouter as Router, Routes, } from "react-router-dom"
+import AddEmployee from "./Component/AddEmployee"
+import Dasboard from "./Component/Dasboard.js/Dasboard"
+import Delete from "./Component/Delete"
+import UpdateEmployee from "./Component/UpdateEmployee"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App(){
+
+
+  return(
+    <>
+    {/* <h1>hello jaipur</h1> */}
+    <Router>
+      <Routes>
+        <Route path="/add" element={<AddEmployee />}   />
+        <Route path="/" element={<Dasboard />}   />
+        <Route path="/delete/:id" element={<Delete />}   />
+        <Route path="/update/:id" element={<UpdateEmployee />}   />
+
+      </Routes>
+    </Router>
+   
+    
+
+    
+    </>
+  )
 }
-
-export default App;
